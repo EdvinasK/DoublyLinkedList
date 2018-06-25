c050ba89a71e786e46e9ffe908a00ad13743b35e
EdvinasK <edvinas.kesminas@gmail.com>
2018-06-25 14:49:50 +0300

    Add project files.

---
+++ DoublyLinkedList/Models/LinkedList.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoublyLinkedList.Models
+{
+    public class LinkedList<T>
+    {
+        public LinkedListNode<T> Head { get; private set; }
+        public LinkedListNode<T> Tail { get; private set; }
+    }
+}
